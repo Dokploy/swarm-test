a1f71e45ddba42fe9a315784efc7c2f313e0c409
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-05-14 22:12:19 -0600

    feat: add express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Generar un identificador único para esta instancia
+const instanceId = Math.random().toString(36).substring(7);
+
+app.get('/', (req, res) => {
+  res.send(`Hello from instance ${instanceId}`);
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
