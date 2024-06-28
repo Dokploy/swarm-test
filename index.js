193668a1f2dd683bcce7bd4752032c949a687cf3
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-06-27 20:49:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = 3000;
 
 // Generar un identificador único para esta instancia
 const instanceId = Math.random().toString(36).substring(7);
-
 app.get('/', (req, res) => {
   res.send(`Hello from instance ${instanceId}`);
 });
