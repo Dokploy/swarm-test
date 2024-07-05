d043cf88658e0b4cf20934148409dcdfb771e5ee
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-05 13:52:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const instanceId = Math.random().toString(36).substring(7);
 app.get('/', (req, res) => {
   res.send(`Hello from instance ${instanceId}`);
 });
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
