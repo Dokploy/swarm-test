c9677ead9a6777bb41740f08c9b5aced6d435ca5
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-12-01 13:42:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000
 
-
 // Generar un identificador único para esta instancia
 const instanceId = Math.random().toString(36).substring(7);
 app.get('/', (req, res) => {
