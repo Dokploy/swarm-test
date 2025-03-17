c62c7284b04437fbdd12d5098d5c0810a70cd686
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2025-03-16 20:00:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const instanceId = Math.random().toString(36).substring(7);
 // Función para agregar un delay
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 // Iniciar con un delay de 5 segundos
-
 (async () => {
   await delay(5000); // Espera 5 segundos
   // Definir las rutas después del delay
